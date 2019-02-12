d4b9e48fe7e0e193c7c97a5d72b727d01b8a3b0f
Abraham Bueno <abueno@localhost.localdomain>
2019-02-12 15:14:36 -0600

    Initial setup

---
+++ src/test/java/com/abrahambueno/javadogs/JavadogsApplicationTests.java
@@ -0,0 +1,17 @@
+package com.abrahambueno.javadogs;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JavadogsApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+

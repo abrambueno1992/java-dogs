3e8fdb2872ce90f82c3bb60e0265b102c614ce2f
Abraham Bueno <abueno@localhost.localdomain>
2019-02-12 17:00:26 -0600

    Program is working, but I need to do everything needed for MVP.

--- src/main/java/com/abrahambueno/javadogs/DogResourceAssembler.java
+++ src/main/java/com/abrahambueno/javadogs/DogResourceAssembler.java
@@ -1,4 +1,17 @@
 package com.abrahambueno.javadogs;
 
-public class DogResourceAssembler {
+import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.ResourceAssembler;
+import org.springframework.stereotype.Component;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
+@Component
+public class DogResourceAssembler implements ResourceAssembler <Dogs, Resource<Dogs>> {
+    @Override
+    public Resource<Dogs> toResource(Dogs dog) {
+        return new Resource<Dogs>(dog,
+//                linkTo(methodOn(DogsController.class).findOne(dog.getId())).withRel(),
+                linkTo(methodOn(DogsController.class).all()).withRel("dogs"));
+    }
 }
--- src/main/java/com/abrahambueno/javadogs/DogsController.java
+++ src/main/java/com/abrahambueno/javadogs/DogsController.java
@@ -1,4 +1,45 @@
 package com.abrahambueno.javadogs;
 
+import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.Resources;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.springframework.hateoas.core.DummyInvocationUtils.methodOn;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+
+@RestController
+@RequestMapping("/dogs")
 public class DogsController {
+    private final DogRepository dogrepos;
+    private final DogResourceAssembler assembler;
+    public DogsController(DogRepository dogsrepos, DogResourceAssembler assembler) {
+        this.dogrepos = dogsrepos;
+        this.assembler = assembler;
+    }
+
+    // Post -> Create
+    // Get -> Read
+    // Put -> Update / Replace
+    // Patch -> Update / Modify (collections)
+    // Delete -> Delete
+
+    // RequestMapping(method = RequestMethod.GET)
+    @GetMapping("/breeds")
+    public Resources<Resource<Dogs>> all() {
+        List<Resource<Dogs>> dogs = dogrepos.findAll().stream()
+                .map(assembler::toResource)
+                .collect(Collectors.toList());
+        return new Resources<>(dogs, linkTo(methodOn(DogsController.class).all()).withSelfRel());
+    }
+
+//    @GetMapping("/weight")
+
+//    @GetMapping("/breeds")
+
+//    @GetMapping("/apartment")
 }

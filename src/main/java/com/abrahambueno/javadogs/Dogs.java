a6699dc69645e4cb8b5c3ddb647980a020cf7c43
Abraham Bueno <abueno@localhost.localdomain>
2019-02-12 15:59:54 -0600

    Adding data to dataBase

--- src/main/java/com/abrahambueno/javadogs/Dogs.java
+++ src/main/java/com/abrahambueno/javadogs/Dogs.java
@@ -1,4 +1,28 @@
 package com.abrahambueno.javadogs;
 
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Data // creates getters, setters, to String
+@Entity // object ready JPA storage
 public class Dogs {
+    private @Id @GeneratedValue Long id;
+    private String name;
+    private double weight;
+    private boolean apartment;
+
+    // needed for JPA
+    public Dogs() {
+        // default contructor
+    }
+
+    public Dogs(String name, double weight, boolean apartment) {
+        this.name = name;
+        this.weight = weight;
+        this.apartment = apartment;
+    }
 }
--- src/main/java/com/abrahambueno/javadogs/SeedDatabase.java
+++ src/main/java/com/abrahambueno/javadogs/SeedDatabase.java
@@ -1,4 +1,22 @@
 package com.abrahambueno.javadogs;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j
+@Configuration
 public class SeedDatabase {
+    @Bean
+    public CommandLineRunner InitDB(DogRepository dogRepository) {
+        return args -> {
+            log.info("Seeding " + dogRepository.save(new Dogs("Springer", 50, false)));
+            log.info("Seeding " + dogRepository.save(new Dogs("Bulldog", 50, true)));
+            log.info("Seeding " + dogRepository.save(new Dogs("Collie", 50, false)));
+            log.info("Seeding " + dogRepository.save(new Dogs("Boston Terrie", 35, true)));
+            log.info("Seeding " + dogRepository.save(new Dogs("Corgie", 35, true)));
+
+        };
+    }
 }
